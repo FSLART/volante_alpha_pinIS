853a8868208e5453d7249f2b1bfbcf68740c945a
Imeguras <imeguras@teknik.io>
2023-09-05 16:06:51 +0100

    Migraition

--- nhlon.ino
+++ nhlon.ino
@@ -14,9 +14,16 @@ float OP = 0;
 int32_t OPint = 0;
 unsigned long millist=0;
 unsigned long milliss=0;
+int GEAR=0;
+typedef union  {
+  int32_t encodedValue;
+  float decodedValue;
+}EncodingUnion;
+
+EncodingUnion g_OilPressure; 
 
 String bsonW="\xFF\xFF\xFF\xFF";
-int period=15;
+int period=90;
 void setup() {
     // Setup serial port
     Serial.begin(115200);
@@ -38,28 +45,34 @@ void loop(){
     
     // Append an element to BSON Builder
     bob.append("rpm", (int32_t)RPM);
-    //Serial.print(RPM); 
-    // Generate BSON Object
+    //bob.append("bat_voltage", (int32_t)g_OilPressure.encodedValue); //float 
+    //bob.append("oil_temp",  (int32_t)g_OilPressure.encodedValue); //float
+    //bob.append("oil_press",  (int32_t)g_OilPressure.encodedValue); //float
+    //bob.append("eng_temp", (int32_t) RPM);
+    //bob.append("velocity", (int32_t) RPM);
+    
     BSONObject bo = bob.obj();
     int a =  bo.len();
     if(millist-milliss>=period){
       Serial.print(bsonW);
-      //Serial.print(a);
-      //Serial.write(bo.rawData(), a);
+ 
       Serial.write(bo.rawData(), a);
       milliss=millist;
     }    
     
     //Serial.print(millis()-millist);
-    POT=analogRead(A0);
-   period=map(POT,0,1024,1,1000);
+    
 }
 void simulateCAN(){
   RPM+=10;
   ET ++;
   OP+=0.1;
-  if(RPM>10000){
+  if(GEAR>=6){
+    GEAR=0;
+  }
+  if(RPM>5000){
     RPM=0;
+    GEAR+=1;
   }
   if(ET>120){
     ET=0;
